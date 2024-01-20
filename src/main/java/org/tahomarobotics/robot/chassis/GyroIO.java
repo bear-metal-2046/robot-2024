3f906c0e3010ec28de6a3286776ffa60c0f9a751
zachhoppe <97768900+zachhoppe@users.noreply.github.com>
2024-01-19 16:51:41 -0800

    Fast odometry (#23)
    * Prototype fast polling
    
    * tested odom thread, plus added ctre things
    
    ---------
    
    Co-authored-by: Avery Black <avery.c.black@gmail.com>

--- src/main/java/org/tahomarobotics/robot/chassis/GyroIO.java
+++ src/main/java/org/tahomarobotics/robot/chassis/GyroIO.java
@@ -1,9 +1,17 @@
 package org.tahomarobotics.robot.chassis;
 
+import com.ctre.phoenix6.BaseStatusSignal;
 import edu.wpi.first.math.geometry.Rotation2d;
+
+import java.util.Collections;
+import java.util.List;
 
 public interface GyroIO {
     default void zero() {}
 
     Rotation2d getYaw();
+
+    default List<BaseStatusSignal> getStatusSignals() {
+        return Collections.emptyList();
+    }
 }

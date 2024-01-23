8a97eda9c15e8050523ee0f346bdb270ac26d942
Nukkel <110308379+nukkeldev@users.noreply.github.com>
2024-01-23 12:52:45 -0800

    Added ALPHA and fixed formatting. (#46)

--- src/main/java/org/tahomarobotics/robot/util/CalibrationData.java
+++ src/main/java/org/tahomarobotics/robot/util/CalibrationData.java
@@ -23,7 +23,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.util.Arrays;
 
+@SuppressWarnings("unchecked")
 public class CalibrationData<T extends Serializable> {
 
     private final static Logger logger = LoggerFactory.getLogger(CalibrationData.class);
@@ -31,7 +33,7 @@ public class CalibrationData<T extends Serializable> {
     private static final File HOME_DIR = Filesystem.getOperatingDirectory();
 
     private final File file;
-    private T data;
+    private T[] data;
 
     /**
      * Take care of reading and writing of calibration data to a file on the robot.
@@ -39,7 +41,7 @@ public class CalibrationData<T extends Serializable> {
      * @param defaultData - data is initialized to these values when no file is found
      */
     public CalibrationData(String filename, T defaultData) {
-        this.data = defaultData;
+        this.data = castData(defaultData);
         this.file = new File(HOME_DIR, filename);
         readCalibrationFile();
     }
@@ -47,17 +49,17 @@ public class CalibrationData<T extends Serializable> {
     @SuppressWarnings("unchecked")
     private void readCalibrationFile() {
         try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file))) {
-            data = (T)inputStream.readObject();
-            logger.info("Successfully read calibration data <" + file.getAbsolutePath() + "> -> " + data);
+            data = castData((T)inputStream.readObject());
+            logger.info("Successfully read calibration data <" + file.getAbsolutePath() + "> -> " + formatData());
         } catch (Exception e) {
             logger.error("Failed to read calibration data <" + file.getAbsolutePath() + ">");
         }
     }
 
-    private void writeCalibrationFile(T data) {
+    private void writeCalibrationFile(T[] data) {
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file))) {
-            outputStream.writeObject(data);
-            logger.warn("Wrote new calibration data <" + file.getAbsolutePath() + "> -> " + data);
+            outputStream.writeObject(data[0]);
+            logger.warn("Wrote new calibration data <" + file.getAbsolutePath() + "> -> " + formatData());
         } catch (Exception e) {
             logger.error("Failed to write calibration data <" + file.getAbsolutePath() + ">", e);
         }
@@ -67,14 +69,23 @@ public class CalibrationData<T extends Serializable> {
      * Returns the calibration data read from file if successful otherwise the default data
      */
     public T get() {
-        return data;
+        return data[0];
     }
 
     /**
      * Applies a new set of data to the configuration file
      */
     public void set(T data) {
-        this.data = data;
-        writeCalibrationFile(data);
+        this.data = castData(data);
+        writeCalibrationFile(this.data);
+    }
+
+    private T[] castData(T data) {
+        return (T[]) new Serializable[]{ data };
+    }
+
+    private String formatData() {
+        String tmp = Arrays.deepToString(data).substring(1);
+        return tmp.substring(0, tmp.length() - 1);
     }
 }

8127e3d5f55c2a96fb91af43a1a3b187d56dd78b
ArcRobotics <jmorrison208@go.tahomasd.us>
2024-01-27 12:25:36 -0800

    tuned pid on the arm

--- src/main/java/org/tahomarobotics/robot/arm/ArmConstants.java
+++ src/main/java/org/tahomarobotics/robot/arm/ArmConstants.java
@@ -1,6 +1,7 @@
 package org.tahomarobotics.robot.arm;
 
 import com.ctre.phoenix6.configs.*;
+import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
@@ -11,19 +12,24 @@ public class ArmConstants {
     static final double AMP_POSE = 0.5; // Rotations
     static final double TRAP_POSE = 0.5; // Rotations
 
+
+
     static final TalonFXConfiguration armMotorConfiguration = new TalonFXConfiguration()
-            .withSlot0(new Slot0Configs()
-                    .withKP(0)
-                    .withKS(0)
-                    .withKV(0)
-                    .withKA(0))
+            .withSlot0(new Slot0Configs() {{
+                GravityType = GravityTypeValue.Arm_Cosine;
+            }}
+                    .withKP(63.666)
+                    .withKD(13.57)
+                    .withKS(0.033307)
+                    .withKV(5.3763)
+                    .withKA(1.5512).withKG(0.17226))
             .withMotorOutput(new MotorOutputConfigs()
                     .withNeutralMode(NeutralModeValue.Brake)
                     .withInverted(InvertedValue.Clockwise_Positive))
             .withMotionMagic(new MotionMagicConfigs()
-                    .withMotionMagicCruiseVelocity(0)
-                    .withMotionMagicAcceleration(0)
-                    .withMotionMagicJerk(0))
+                    .withMotionMagicCruiseVelocity(1)
+                    .withMotionMagicAcceleration(10)
+                    .withMotionMagicJerk(50))
             .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(1 / ARM_GEAR_REDUCTION))
             .withAudio(new AudioConfigs().withBeepOnBoot(true).withBeepOnConfig(true));
 }

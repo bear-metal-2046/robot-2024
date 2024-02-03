6e0bcfebaa269426c339c50779b6a44dcbf6c5a9
nukkeldev <nukkel.dev@gmail.com>
2024-02-03 13:44:02 -0800

    Debugging the commands

--- src/main/java/org/tahomarobotics/robot/amp/commands/AmpArmCommands.java
+++ src/main/java/org/tahomarobotics/robot/amp/commands/AmpArmCommands.java
@@ -2,6 +2,8 @@ package org.tahomarobotics.robot.amp.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.tahomarobotics.robot.amp.AmpArm;
 import org.tahomarobotics.robot.indexer.Indexer;
 import org.tahomarobotics.robot.shooter.Shooter;
@@ -12,6 +14,8 @@ import java.util.function.Supplier;
 import static org.tahomarobotics.robot.amp.AmpArmConstants.*;
 
 public class AmpArmCommands {
+    private static final Logger logger = LoggerFactory.getLogger(AmpArmCommands.class);
+
     private static final Supplier<Command> FEEDFORWARD;
     private static final Supplier<Command> FEEDBACK;
     private static final Supplier<Command> STOW_TO_AMP;
@@ -34,7 +38,7 @@ public class AmpArmCommands {
                 Commands.runOnce(() -> ampArm.setArmPosition(ARM_STOW_POSE)),
                 Commands.waitUntil(ampArm::isArmAtPosition),
                 Commands.runOnce(() -> ampArm.setArmState(AmpArm.ArmState.STOW))
-        ).onlyIf(ampArm::isAmp);
+        );
 
         STOW_TO_SOURCE = () -> Commands.sequence(
                 Commands.runOnce(() -> ampArm.setWristPosition(WRIST_MOVING_POSE)),
@@ -89,14 +93,19 @@ public class AmpArmCommands {
         Shooter shooter = Shooter.getInstance();
 
         AMP_ARM_CTRL = Commands.deferredProxy(() -> {
-            if ((ampArm.isSource() || ampArm.isAmp()) && ampArm.isCollected())
+            if ((ampArm.isSource() || ampArm.isAmp()) && ampArm.isCollected()) {
+                logger.info("Running Arm to Stow then Feedback...");
                 return Commands.defer(() -> ARM_TO_STOW.get().andThen(FEEDBACK.get()), Set.of(ampArm, indexer, shooter));
-            if (ampArm.isAmp() || ampArm.isSource())
+            } if (ampArm.isAmp() || ampArm.isSource()) {
+                logger.info("Running Arm to Stow...");
                 return Commands.defer(ARM_TO_STOW, Set.of(ampArm));
-            if (indexer.hasCollected())
+            } if (indexer.hasCollected()) {
+                logger.info("Running Feedforward then Stow to Amp...");
                 return Commands.defer(() -> FEEDFORWARD.get().andThen(STOW_TO_AMP.get()), Set.of(ampArm, indexer, shooter));
-            else
+            } else {
+                logger.info("Running Stow to Source...");
                 return Commands.defer(STOW_TO_SOURCE, Set.of(ampArm));
+            }
         });
     }
 }

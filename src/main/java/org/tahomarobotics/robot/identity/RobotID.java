b43aeba2d4b46640504904d55e32020e3b4cc770
nukkeldev <nukkel.dev@gmail.com>
2024-02-20 12:00:37 -0800

    Added Playbear Carti's MAC and switched it to be default.

--- src/main/java/org/tahomarobotics/robot/identity/RobotID.java
+++ src/main/java/org/tahomarobotics/robot/identity/RobotID.java
@@ -1,7 +1,6 @@
 package org.tahomarobotics.robot.identity;
 
 public enum RobotID {
-    //Build season alpha bot
     ALPHA {
         @Override
         public byte[] getMac() {
@@ -9,23 +8,14 @@ public enum RobotID {
         }
     },
 
-    CHASSIS {
+    PLAYBEAR_CARTI {
         @Override
         public byte[] getMac() {
             return new byte[]{0x00, (byte) 0x80, 0x2F, 0x33, 0x04, (byte) 0xF9};
         }
     },
 
-    //One of the two comp bots, rename accordingly later
-    PRACTICE {
-        @Override
-        public byte[] getMac() {
-            return new byte[]{};
-        }
-    },
-
-    //Primary comp bot, rename accordingly later
-    COMPETITION {
+    BEARITONE {
         @Override
         public byte[] getMac() {
             return new byte[]{};

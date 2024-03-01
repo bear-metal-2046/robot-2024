2d3abd99379bed8761e2615604a855d9bb28e795
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2024-02-29 16:55:00 -0800

    ClimbSequence changes

--- src/main/java/org/tahomarobotics/robot/climbers/commands/ClimbSequence.java
+++ src/main/java/org/tahomarobotics/robot/climbers/commands/ClimbSequence.java
@@ -5,24 +5,27 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ProxyCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import org.tahomarobotics.robot.amp.commands.AmpArmCommands;
+import org.tahomarobotics.robot.chassis.Chassis;
 import org.tahomarobotics.robot.chassis.ChassisConstants;
 import org.tahomarobotics.robot.chassis.commands.DriveForwardCommand;
 import org.tahomarobotics.robot.shooter.Shooter;
 import org.tahomarobotics.robot.shooter.ShooterConstants;
+
+import java.util.Set;
 
 import static org.tahomarobotics.robot.climbers.ClimberConstants.*;
 
 public class ClimbSequence extends SequentialCommandGroup {
     public ClimbSequence() {
         super(
-                new ProxyCommand(AmpArmCommands.ARM_TO_STOW.get()),
                 Commands.parallel(
-                    new ClimbCommand(TOP_POSITION, UNLADEN_SLOT),
                         Commands.runOnce(() -> Shooter.getInstance().setAngle(ShooterConstants.MAX_PIVOT_ANGLE)),
-                        new ProxyCommand(AutoBuilder.pathfindToPose(ChassisConstants.getClosestChainPose(), ChassisConstants.CLIMB_MOVEMENT_CONSTRAINTS))
+                        Commands.defer(() -> AutoBuilder.pathfindToPose(ChassisConstants.getClosestChainPose(), ChassisConstants.CLIMB_MOVEMENT_CONSTRAINTS), Set.of(Chassis.getInstance()))
                 ),
-                new DriveForwardCommand(0.5/3.0, 1),
+                new ClimbCommand(TOP_POSITION, UNLADEN_SLOT),
+                new DriveForwardCommand(0.1666666666666, 1),
                 AmpArmCommands.AMP_ARM_CLIMB.get(),
                 new ClimbCommand(BOTTOM_POSITION, LADEN_SLOT)
         );

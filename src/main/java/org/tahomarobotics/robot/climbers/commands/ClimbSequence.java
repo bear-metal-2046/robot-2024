fa7c25411bc1a904daca66fc400463999cda41d9
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2024-02-29 16:57:53 -0800

    Added ARM_TO_STOW at beginning of ClimbSequence

--- src/main/java/org/tahomarobotics/robot/climbers/commands/ClimbSequence.java
+++ src/main/java/org/tahomarobotics/robot/climbers/commands/ClimbSequence.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ProxyCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import org.tahomarobotics.robot.amp.AmpArm;
 import org.tahomarobotics.robot.amp.commands.AmpArmCommands;
 import org.tahomarobotics.robot.chassis.Chassis;
 import org.tahomarobotics.robot.chassis.ChassisConstants;
@@ -20,12 +21,13 @@ import static org.tahomarobotics.robot.climbers.ClimberConstants.*;
 public class ClimbSequence extends SequentialCommandGroup {
     public ClimbSequence() {
         super(
+                Commands.defer(AmpArmCommands.ARM_TO_STOW, Set.of(AmpArm.getInstance())),
                 Commands.parallel(
                         Commands.runOnce(() -> Shooter.getInstance().setAngle(ShooterConstants.MAX_PIVOT_ANGLE)),
                         Commands.defer(() -> AutoBuilder.pathfindToPose(ChassisConstants.getClosestChainPose(), ChassisConstants.CLIMB_MOVEMENT_CONSTRAINTS), Set.of(Chassis.getInstance()))
                 ),
                 new ClimbCommand(TOP_POSITION, UNLADEN_SLOT),
-                new DriveForwardCommand(0.1666666666666, 1),
+                new DriveForwardCommand(0.2, 1),
                 AmpArmCommands.AMP_ARM_CLIMB.get(),
                 new ClimbCommand(BOTTOM_POSITION, LADEN_SLOT)
         );

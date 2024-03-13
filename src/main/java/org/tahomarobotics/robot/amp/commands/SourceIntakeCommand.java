bfcb227e91af9ff4c5ff8d56d375c1cbaf13638b
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2024-03-12 18:26:24 -0700

    AmpArm Automatically centers the note after detecting a note now

--- src/main/java/org/tahomarobotics/robot/amp/commands/SourceIntakeCommand.java
+++ src/main/java/org/tahomarobotics/robot/amp/commands/SourceIntakeCommand.java
@@ -21,6 +21,7 @@ public class SourceIntakeCommand extends SequentialCommandGroup {
             Commands.waitUntil(() -> (ampArm.getRollerCurrent() > AmpArmConstants.SOURCE_COLLECT_CURRENT && timer.hasElapsed(AmpArmConstants.SOURCE_COLLECT_ACCEL_TIME*2)) || !trigger.getAsBoolean()), Commands.runOnce(() -> ampArm.setRollerState(AmpArm.RollerState.DISABLED)),
                 Commands.runOnce(() -> ampArm.setRollerState(AmpArm.RollerState.DISABLED)),
                 Commands.sequence(
+                        Commands.runOnce(() -> OI.getInstance().log("DETECTED NOTE")),
                 Commands.runOnce(ampArm::sourceIntake),
                 Commands.waitUntil(ampArm::isRollerAtPosition),
                     Commands.runOnce(() -> ampArm.setRollerState(AmpArm.RollerState.COLLECTED))

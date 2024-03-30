aa6f3ff97e7add7c9223dfb66169c4b112c51d5b
zachhoppe <zach.hoppe06@gmail.com>
2024-03-30 12:11:13 -0700

    subtracted 180 from all wrist poses

--- src/main/java/org/tahomarobotics/robot/amp/AmpArmConstants.java
+++ src/main/java/org/tahomarobotics/robot/amp/AmpArmConstants.java
@@ -61,10 +61,10 @@ public class AmpArmConstants {
     static {
         switch (RobotIdentity.robotID) {
             case PLAYBEAR_CARTI, BEARITONE -> {
-                WRIST_MOVING_POSE = Units.degreesToRotations(123.22265625);
-                WRIST_AMP_POSE = Units.degreesToRotations(145.7421875 + 3.0);
-                WRIST_TRAP_POSE = Units.degreesToRotations(139.314453125 + 10.0);
-                WRIST_SOURCE_POSE = Units.degreesToRotations(76.2890625);
+                WRIST_MOVING_POSE = Units.degreesToRotations(123.22265625 - 180);
+                WRIST_AMP_POSE = Units.degreesToRotations(145.7421875 + 3.0 - 180);
+                WRIST_TRAP_POSE = Units.degreesToRotations(139.314453125 + 10.0 - 180);
+                WRIST_SOURCE_POSE = Units.degreesToRotations(76.2890625 - 180);
             }
             default -> {
                 WRIST_MOVING_POSE = Units.degreesToRotations(67.67578125);

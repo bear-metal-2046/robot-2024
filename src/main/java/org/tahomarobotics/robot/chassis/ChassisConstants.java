13123e143af01578b4cad6549fad302fc0cb49d5
zachhoppe <zach.hoppe06@gmail.com>
2024-04-05 18:21:07 -0700

    6.0 drive acceleration

--- src/main/java/org/tahomarobotics/robot/chassis/ChassisConstants.java
+++ src/main/java/org/tahomarobotics/robot/chassis/ChassisConstants.java
@@ -97,7 +97,7 @@ public final class ChassisConstants {
     public static final double MAX_VELOCITY = SWERVE_DRIVE_MOTOR.freeSpeedRadPerSec * DRIVE_REDUCTION * WHEEL_RADIUS * 1.1; // m/s
     public static final double MAX_ANGULAR_VELOCITY = MAX_VELOCITY / Math.hypot(HALF_TRACK_WIDTH, HALF_WHEELBASE); // r/s
 
-    public static final double ACCELERATION_LIMIT = 5.0;
+    public static final double ACCELERATION_LIMIT = 6.0;
     public static final double TRANSLATION_LIMIT = ACCELERATION_LIMIT;
     public static final double ROTATION_LIMIT = TRANSLATION_LIMIT / Math.hypot(HALF_TRACK_WIDTH, HALF_WHEELBASE);
 
